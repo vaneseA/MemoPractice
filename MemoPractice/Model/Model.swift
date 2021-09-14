c8832078e6ffc0384a1a2802bf4c9c97cd1ca835
DK <spol@naver.com>
2021-09-14 17:44:58 +0900

    메모 클래스

---
+++ MemoPractice/Model/Model.swift
@@ -0,0 +1,25 @@
+//
+//  Model.swift
+//  MemoPractice
+//
+//  Created by DK on 2021/09/14.
+//
+
+import Foundation
+
+
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    init(content: String){
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content:  "Lorem Ipsum"),
+        Memo(content:  "Subscribe + e = 2")
+          
+        ]
+}
