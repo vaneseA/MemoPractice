14e7f94772d3bc017811b038727a156d82fba14c
DK <spol@naver.com>
2021-09-24 22:46:43 +0900

    메모편집

--- MemoPractice/DetailViewController.swift
+++ MemoPractice/DetailViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var memoTableView: UITableView!
+    
     var memo: Memo?
     
     let formatter: DateFormatter = {
@@ -19,8 +21,26 @@ class DetailViewController: UIViewController {
         return f
     }()
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination.children.first as?ComposeViewController{
+            vc.editTarget = memo
+        }
+    }
+    
+    var token: NSObjectProtocol?
+    
+    deinit {
+        if let token = token {
+            NotificationCenter.default.removeObserver(token)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        token = NotificationCenter.default.addObserver(forName: ComposeViewController.memoDidChange, object: nil, queue: OperationQueue.main, using: { [weak self] (noti) in self?.memoTableView.reloadData()
+            
+        })
 
         // Do any additional setup after loading the view.
     }
